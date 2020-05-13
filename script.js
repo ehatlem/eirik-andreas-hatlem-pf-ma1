e40b42ebe584a1db9dc887e0f0ca58f701291174
ehatlem <59869903+ehatlem@users.noreply.github.com>
2020-05-13 21:11:17 +0200

    Upload script.js for ma1

---
+++ script.js
@@ -0,0 +1,37 @@
+// question 1
+
+var firstVar;
+
+// question 2
+
+var name = "Eirik";
+
+// question 3
+
+var number = 14;
+
+// question 4
+
+var division = 20 / 5;
+
+// question 5
+
+console.log(typeof ("frog"));
+
+// question 6
+
+var orderHasShipped = false;
+
+// question 7
+
+if (orderHasShipped) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// question 8
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
